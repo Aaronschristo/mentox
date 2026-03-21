730b3c66947a016de333e0d66cd8fd027ba91d7e
Aaron S Christo <aronschristo@gmail.com>
2026-03-21 15:35:19 +0530

    feat: transformed the flask application into a WSGI server suitable for production.

--- app.py
+++ app.py
@@ -171,4 +171,5 @@ def generate_qr(customer_id):
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5000)
