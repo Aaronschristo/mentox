e786f83686ea6aabb2509c1f55dd76f03db6c0a2
Aaronschristo <89347296+Aaronschristo@users.noreply.github.com>
2026-04-17 04:05:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        if (targetId === '#') return;
+        
+        const target = document.querySelector(targetId);
+        if(target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
